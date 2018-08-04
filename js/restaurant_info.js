34e3903549b1be67d6104e9186f37120a1c3e9b9
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2018-08-04 14:26:46 +0300

    Change image alt

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.alt;
+  image.alt = 'Image of ' + restaurant.name + " restaurant";
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
